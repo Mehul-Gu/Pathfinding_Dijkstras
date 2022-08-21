833783daf517d416cc5dac33581c6cde3602ee7e
Mehul-Gu <mehuld@utexas.edu>
2022-08-21 02:04:33 -0500

    adding pathfinding folders

---
+++ pathfinding/src/pathfinding/block/block.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+import './Block.css';
+
+export default class Block extends Component {
+    constructor(props) {
+        super(props);
+        this.props = {}
+    }
+}
---
+++ pathfinding/src/pathfinding/pathfinding.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Pathfinding() {
+    return <h1>Hello Mehul</h1>
+}
+
+export default Pathfinding;
