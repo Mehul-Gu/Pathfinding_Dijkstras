c3b9d32daf85cd17c82da75db2952d8985e1fa28
Mehul-Gu <mehuld@utexas.edu>
2022-08-21 02:08:52 -0500

    fixing repo

---
+++ pathfinding/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Pathfinding from './pathfinding/pathfinding';
+
+function App() {
+  return (
+    <div className="App">
+      <Pathfinding></Pathfinding>
+    </div>
+  );
+}
+
+export default App;
